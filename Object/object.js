381e6ed6e1c410d229dbcc18a2268f5c82091fd3
Abdelmn3m mohamed <abdel_moneim122@hotmail.com>
2019-03-21 03:24:07 +0200

    test this keyword behavior

--- Object/object.js
+++ Object/object.js
@@ -1,13 +1,16 @@
-
-// object literal 
-// create other instance will case copy 
+// object literal
+// create other instance will case copy
 const Circle = {
-    radiues: 1 ,
-    location : {
-        x: 1 ,
-        y: 1
-    },
-    draw: function () {
-        console.log('draw');
-    }
-}
+  radiues: 1,
+  location: {
+    x: 1,
+    y: 1
+  },
+  draw: function() {
+    console.log(this);
+  }
+};
+
+const draw = Circle.draw;
+draw();
+console.log(draw);
